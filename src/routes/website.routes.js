f3574bfc9778e8d528b0d5bf0d24e8442057f138
Ahmed Khamis <ahmeddkhamiess10@gmail.com>
2025-05-26 06:02:20 +0300

    cleaning some comments
    Signed-off-by: Ahmed Khamis <ahmeddkhamiess10@gmail.com>

--- src/routes/website.routes.js
+++ src/routes/website.routes.js
@@ -1,88 +1,18 @@
 const express = require("express");
 const { login, signup, confirm } = require("../controllers/auth.controller");
-
-const { YOUR_APP_CLIENT_ID, YOUR_USER_POOL_ID } = require("../config");
-const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
 const { authMiddleware } = require("../middlewares/authentication.middlewares");
-
-
-// const fetch = require("node-fetch");
-// global.fetch = fetch;
-
-// const jwkToPem = require("jwk-to-pem");
-// const jwt = require("jsonwebtoken");
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const jwtAuth = require("express-jwt");
 
 const authRouter =express.Router();
 
-// const poolData = {
-//   UserPoolId: YOUR_USER_POOL_ID,
-//   ClientId: YOUR_APP_CLIENT_ID,
-// };
-// const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
 authRouter.post("/signup",signup)
 
 authRouter.post("/confirm",confirm)
-// authRouter.post("/confirm", (req, res) => {
-//   const { username, code } = req.body;
 
-//   const userData = {
-//     Username: username,
-//     Pool: userPool,
-//   };
-
-//   const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-
-//   cognitoUser.confirmRegistration(code, true, (err, result) => {
-//     if (err) {
-//       return res.status(400).json({ error: err.message });
-//     }
-
-//     res.status(200).json({ message: "Email verified successfully", result });
-//   });
-// });
 authRouter.post("/login", login)
 
-// authRouter.post("/login", (req, res) => {
-//   const { username, password } = req.body;
-
-//   const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
-//     {
-//       Username: username,
-//       Password: password,
-//     }
-//   );
-
-//   const userData = {
-//     Username: username,
-//     Pool: userPool,
-//   };
-
-//   const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-
-//   cognitoUser.authenticateUser(authenticationDetails, {
-//     onSuccess: (result) => {
-//       res.status(200).json({
-//         accessToken: result.getAccessToken().getJwtToken(),
-//         idToken: result.getIdToken().getJwtToken(),
-//         refreshToken: result.getRefreshToken().getToken(),
-//       });
-//     },
-//     onFailure: (err) => {
-//       if (err.code === "UserNotConfirmedException") {
-//         return res
-//           .status(403)
-//           .json({ error: "Please verify your email first." });
-//       }
-
-//       res.status(401).json({ error: err.message });
-//     
-//   });
-// });
 authRouter.get("/test",authMiddleware,(req,res)=>{
     res.json("hello")
 })
+
 module.exports={authRouter}
